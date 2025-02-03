a2b0cddc7a857a9b1e5c83224b472f256e18646c
Armand Collin <armac@videotron.ca>
2025-02-03 17:30:49 -0500

    Implement logic for size sampling

--- scripts/preprocess_image.py
+++ scripts/preprocess_image.py
@@ -10,9 +10,23 @@ from pathlib import Path
 import argparse
 import numpy as np
 from AxonDeepSeg.ads_utils import imread, imwrite
+from skimage.transform import resize
 
-def main()
-    ...
+def get_sizes(image, lower, upper, N):
+    # the first half of the sizes are decreasing, the second half increasing
+    sizes = []
+    for i in range(N//2):
+        factor_decreasing = 1 - (i+1) * (1 - lower) / (N//2)
+        factor_increasing = 1 + (i+1) * (upper - 1) / (N//2)
+        sizes.append((int(image.shape[0] * factor_decreasing), int(image.shape[1] * factor_decreasing)))
+        sizes.append((int(image.shape[0] * factor_increasing), int(image.shape[1] * factor_increasing)))
+    return sizes
+
+def main(input, gt, num_samples, lower, upper):
+    img = imread(input)
+    sizes = get_sizes(img, lower, upper, num_samples)
+    print(img.shape)
+    print(sizes)
 
 if __name__ == '__main__':
     ap = argparse.ArgumentParser()
@@ -21,3 +35,6 @@ if __name__ == '__main__':
     ap.add_argument("-n", "--num-samples", required=True, help="number of times to resize the image") 
     ap.add_argument("-l", "--lower", default=0.5, help="lower bound of the resizing factor")
     ap.add_argument("-u", "--upper", default=3.0, help="upper bound of the resizing factor")
+
+    args = ap.parse_args()
+    main(args.input, args.gt, int(args.num_samples), args.lower, args.upper)
