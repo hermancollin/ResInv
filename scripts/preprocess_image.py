f45f7e17661c5f6671336d8b4e67cacd70111c09
Armand Collin <armac@videotron.ca>
2025-02-04 14:24:25 -0500

    Refactor to process both axon and myelin masks

--- scripts/preprocess_image.py
+++ scripts/preprocess_image.py
@@ -20,21 +20,64 @@ def get_sizes(image, lower, upper, N):
         factor_increasing = 1 + (i+1) * (upper - 1) / (N//2)
         sizes.append((int(image.shape[0] * factor_decreasing), int(image.shape[1] * factor_decreasing)))
         sizes.append((int(image.shape[0] * factor_increasing), int(image.shape[1] * factor_increasing)))
-    return sizes
+    return sorted(sizes)
 
-def main(input, gt, num_samples, lower, upper):
-    img = imread(input)
+def expand_image(impath, num_samples, lower, upper):
+    '''output file structure is as follows:
+        IMAGE.png
+        IMAGE_seg-axon-manual.png
+        IMAGE_seg-myelin-manual.png
+        IMAGE_expansion                 <<< output_dir
+        ├── imgs
+        │   ├── im_256_256.png
+        │   ├── ...
+        ├── gts
+        │   ├── gt-axon_256_256.png
+        │   ├── gt-myelin_256_256.png
+        │   ├── ...
+        ├── preds
+        │   ├── model-1
+        │   │   ├── im_256_256_seg-axon.png
+        │   │   ├── im_256_256_seg-myelin.png
+        │   │   ├── ...
+        │   ├── ...
+    '''
+    img = imread(impath)
+    axon_path = impath.replace(".png", "_seg-axon-manual.png")
+    myelin_path = impath.replace(".png", "_seg-myelin-manual.png")
+    axon_gt = imread(axon_path)
+    myelin_gt = imread(myelin_path)
     sizes = get_sizes(img, lower, upper, num_samples)
-    print(img.shape)
-    print(sizes)
+
+    print(f'Source shape: {img.shape}')
+    print(f'The image will be resized to the following target shapes: {sizes}')
+
+    output_dir = Path(impath).parent / f'{Path(impath).stem}_expansion'
+    output_dir.mkdir(exist_ok=True)
+    imgs_output_dir = output_dir / 'imgs'
+    gts_output_dir = output_dir / 'gts'
+    imgs_output_dir.mkdir(exist_ok=True)
+    gts_output_dir.mkdir(exist_ok=True)
+
+    for size in sizes:
+        new_image = resize(img, size, preserve_range=True)
+        new_axon_gt = resize(axon_gt, size, order=0, preserve_range=True, anti_aliasing=False)
+        new_myelin_gt = resize(myelin_gt, size, order=0, preserve_range=True, anti_aliasing=False)
+        
+        imwrite(imgs_output_dir / f'im_{size[0]}_{size[1]}.png', new_image)
+        imwrite(gts_output_dir / f'gt-axon_{size[0]}_{size[1]}.png', new_axon_gt)
+        imwrite(gts_output_dir / f'gt-myelin_{size[0]}_{size[1]}.png', new_myelin_gt)
+
+def main(impath, num_samples, lower, upper):
+    expand_image(impath, num_samples, lower, upper)
+    
 
 if __name__ == '__main__':
     ap = argparse.ArgumentParser()
-    ap.add_argument("-i", "--input", required=True, help="path to input image")
-    ap.add_argument("-g", "--gt", required=True, help="path to input GT label")
-    ap.add_argument("-n", "--num-samples", required=True, help="number of times to resize the image") 
-    ap.add_argument("-l", "--lower", default=0.5, help="lower bound of the resizing factor")
-    ap.add_argument("-u", "--upper", default=3.0, help="upper bound of the resizing factor")
+    ap.add_argument("-i", "--impath", required=True, help="path to impath image. We assume the corresponding labels are in the same directory and have the same name with suffixes '_seg-axon-manual' and '_seg-myelin-manual'")
+    ap.add_argument("-n", "--num-samples", required=True, help="number of times to resize the image")
+    ap.add_argument("-l", "--lower", default=0.25, help="lower bound of the resizing factor")
+    ap.add_argument("-u", "--upper", default=4.0, help="upper bound of the resizing factor")
 
     args = ap.parse_args()
-    main(args.input, args.gt, int(args.num_samples), args.lower, args.upper)
+    main(args.impath, int(args.num_samples), args.lower, args.upper)
